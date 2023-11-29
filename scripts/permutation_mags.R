069eb2ea3f14f0812bc6dd10b8685489a1c55f5d
SShekarriz <shahrokh.shekarriz@gmail.com>
2023-11-29 15:35:13 -0500

    upating all files

---
+++ scripts/permutation_mags.R
@@ -0,0 +1,249 @@
+#!/usr/bin/env Rscript
+#Shekarriz Sep11,2023
+
+library(tidyverse)
+library(tidytext)
+
+mapfile <- read.csv("../data/UCFMT1_METAmap.txt", sep = "\t")
+# map information for contigs and bins
+c_inBin <- read.csv("../data/Assembly/contigs_inBins.txt", sep = "\t", header = F)
+c_inMAG <- read.csv("../data/Assembly/contigs_inMAGs.txt", sep = "\t", header = F)
+colnames(c_inBin) <- c("contig", "bin_id")
+colnames(c_inMAG) <- c("contig", "bin_id")
+
+c_inMAG %>% select(bin_id) %>% distinct() %>% pull() -> MAGs
+
+
+## reading MAG coverage
+
+heads_cover <- c("sample.id",
+                 "contig", "startpos", "endpos", 
+                 "numreads", "covbases", "coverage", "meandepth",
+                 "meanbaseq", "meanmapq",
+                 "ReadSampling", "Quality")
+
+path="../data/perfect_mapping/DBD"
+patt="_perfect_.*.cover"
+data.frame(sample.id = paste(dir(path,
+                                 pattern = patt))) %>%
+  # read each file from the directory (current path) and then unnest
+  mutate(file_contents = map(sample.id, ~ read.csv(file.path(path, .),
+                                                   sep = "\t", header = F,
+                                                   comment.char = "#"))) %>%
+  unnest() %>% 
+  mutate(sample.id = gsub("_perfect_BDB.cover", "", sample.id)) -> marker_lvl_RA
+colnames(marker_lvl_RA) <- heads_cover
+
+marker_lvl_RA %>%
+  left_join(c_inBin) %>%
+  mutate(bin_q= case_when(bin_id %in% MAGs ~ paste("MAG"),
+                          TRUE ~ paste("Bin"))) %>%
+  filter(bin_q == "MAG")  %>% select(-bin_q) %>%
+  dplyr::rename(sample=sample.id) %>%
+  group_by(sample, bin_id) %>%
+  summarise(bin_len= sum(endpos),
+            bin_covbases=sum(covbases),
+            bin_numreads=sum(numreads),
+            bin_coverage=bin_covbases/bin_len*100) -> marker_lvl_RA
+
+marker_lvl_RA %>%
+  filter(str_detect(sample, "PMCL")) %>%
+  select(sample, bin_id, bin_coverage) %>%
+  spread(sample, bin_coverage) %>%
+  rename(Marker= bin_id)-> marker_lvl_RA
+
+cutoff = 90
+
+
+## Functions
+
+# Make the marker data frame long
+mark_to_long = function(marker_lvl) {
+  # This can be done once. It doesn't need to be repeated each time
+  # convert marker-lvl to long format and make StudyID variable to match mapfile
+  # and select only donor's strains
+  marker_lvl %>%
+    gather(sample, abundance, -Marker) %>%
+    mutate(Study_ID = case_when(
+      str_detect(sample,"PMCL") ~ paste(gsub("_.*", "",sample)),
+      TRUE ~ paste(sample))) -> long_markerlvl
+  return(long_markerlvl)
+}
+
+################################################################################
+# a function to detect common engraftment based on strains (metaphlan markers)
+################################################################################
+get_engraft <- function(long_markerlvl, mapfile, cutoff=cutoff, ...){
+  # complete list of engrafted
+  engraft <- (long_markerlvl 
+              %>% left_join(mapfile) 
+              %>% filter(...)
+              %>% spread(Timepoint, abundance, fill = 0) 
+              %>% filter(WK0 < 25 & WK6 >= cutoff) 
+              %>% select(Marker, Study_ID, WK6) 
+              %>% spread(Study_ID, WK6, fill = 0) 
+              %>% gather(Study_ID, WK6, -Marker)
+              %>% left_join(mapfile))
+  return(engraft)
+}
+
+count_engraft <- function(engraft, group_cols){
+  # Count how many markers engrafted in how many patients
+  g1 = group_cols
+  g2 = c('Marker', group_cols)
+  ct_df = (engraft
+           %>% group_by(.[,g2])
+           %>% summarize(n = sum(WK6 > 0))
+           %>% pivot_wider(names_from = {{ g1 }},
+                           values_from = n)
+           %>% ungroup()
+           %>% mutate(Total = rowSums(across(2:ncol(.)))))
+  # Uncomment the next bit if you want the cumulative sums, but it adds 0.8s
+  # to the run time!
+  # ct_df = (ct_df
+  #          %>% mutate(n = rev(cumsum(rev(n))))) 
+  return(ct_df)
+  
+}
+################################################################################
+
+## permutation of treatment FMT vs placebo (n=12)
+# Number of permutations
+num_permutations <- 1000
+it_res = list()
+# Remove unnecessary columns from the mapfile to speed up 
+# the loop (saves 10s per loop)
+map_mod = (mapfile
+           %>% select(Study_ID, Timepoint, Treatment, Fig_lab))
+# Converting the markers to long format only needs to be done once, saves 5s per
+# loop
+markers_long = mark_to_long(marker_lvl_RA)
+for (i in 1:num_permutations) {
+  # Permute the Treatment column
+  permuted_data <- (map_mod
+                    %>% select(Fig_lab, Treatment)
+                    %>% unique()
+                    %>% mutate(Treatment = sample(Treatment, replace = FALSE))
+                    %>% right_join(select(map_mod, -Treatment))
+                    %>% select(-Fig_lab)) # don't need this anymore
+  it_res[[i]] = list()
+  it_res[[i]][['counts']] = (markers_long
+                             %>% get_engraft(permuted_data, cutoff, 
+                                             Treatment %in% c('FMT', 'Placebo'))
+                             %>% count_engraft(group_cols = 'Treatment')
+                             %>% mutate(Uniqueness = 
+                                          case_when(FMT == 0 & Placebo == 0 ~ 'zero',
+                                                    FMT == 0 ~ 'Placebo',
+                                                    Placebo == 0 ~ 'FMT',
+                                                    TRUE ~ 'Both'),
+                                        it = paste('it', i, sep = '_')))
+  it_res[[i]][['Npts_tx']] = rbind(
+    (it_res[[i]][['counts']]
+     %>% count(FMT)
+     %>% rename(Npts = FMT)
+     %>% mutate(Treatment = 'FMT')),
+    (it_res[[i]][['counts']]
+     %>% count(Placebo)
+     %>% rename(Npts = Placebo)
+     %>% mutate(Treatment = 'Placebo')))
+  it_res[[i]][['Npts_total']] = (it_res[[i]][['counts']]
+                                 %>% count(Total, Uniqueness)
+                                 %>% rename(Npts = Total)
+                                 %>% mutate(Treatment = 'Total'))
+}
+
+### Permuted AUC
+#approxfun creates an approximate function from the data using imputation
+#integrate performs numerical integration to get the area under the curve
+auc_null = matrix(NA, ncol = 2, nrow = num_permutations)
+colnames(auc_null) = c('deltaAUC','iteration')
+
+for (i in 1:num_permutations){
+  fmt_fn = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'FMT'),
+                approxfun(Npts, n))
+  plc_fn = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'Placebo'),
+                approxfun(Npts, n))
+  fmt_auc = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'FMT'),
+                 integrate(fmt_fn, 1, max(Npts))$value)
+  plc_auc = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'Placebo'),
+                 integrate(plc_fn, 1, max(Npts))$value)
+  d_auc = fmt_auc - plc_auc 
+  auc_null[i,] = c(d_auc, i)
+  
+}
+write.table(auc_null, "../permut_data/mags_auc_null.txt",
+            sep = "\t", row.names = F, quote = F)
+
+
+## permutations of responders vs non-responders within FMT
+# Number of permutations
+num_permutations <- 1000
+
+fmt_res = list()
+
+# Remove unnecessary columns from the mapfile to speed up the loop (saves 10s per 
+# loop)
+map_mod = (mapfile
+           %>% filter(Treatment == 'FMT')
+           %>% select(Study_ID, Timepoint, Remission, Fig_lab))
+# Loop through permutations
+for (i in 1:num_permutations) {
+  # Permute the Treatment column
+  permuted_data <- (map_mod
+                    %>% select(Fig_lab, Remission)
+                    %>% unique()
+                    %>% mutate(Remission = sample(Remission, replace = FALSE))
+                    %>% right_join(select(map_mod, -Remission))
+                    %>% select(-Fig_lab)) # don't need this anymore
+  fmt_res[[i]] = list()
+  fmt_res[[i]][['counts']] = (markers_long
+                              %>% get_engraft(permuted_data, cutoff, 
+                                              Remission %in% c('Res', 'NoRes'))
+                              %>% count_engraft(group_cols = 'Remission')
+                              %>% mutate(Uniqueness = 
+                                           case_when(Res == 0 & NoRes == 0 ~ 'zero',
+                                                     Res == 0 ~ 'NoRes',
+                                                     NoRes == 0 ~ 'Res',
+                                                     TRUE ~ 'Both'),
+                                         it = paste('it', i, sep = '_')))
+  fmt_res[[i]][['Npts_tx']] = rbind(
+    (fmt_res[[i]][['counts']]
+     %>% count(Res)
+     %>% rename(Npts = Res)
+     %>% mutate(Remission = 'Res')),
+    (fmt_res[[i]][['counts']]
+     %>% count(NoRes)
+     %>% rename(Npts = NoRes)
+     %>% mutate(Remission = 'NoRes')))
+  it_res[[i]][['Npts_total']] = (fmt_res[[i]][['counts']]
+                                 %>% count(Total, Uniqueness)
+                                 %>% rename(Npts = Total)
+                                 %>% mutate(Remission = 'Total'))
+  
+}
+
+### Permuted AUCls -
+#approxfun creates an approximate function from the data using imputation
+#integrate performs numerical integration to get the area under the curve
+
+rm_auc_null = matrix(NA, ncol = 2, nrow = num_permutations)
+colnames(rm_auc_null) = c('deltaAUC','iteration')
+
+for (i in 1:num_permutations){
+  res_fn = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'Res'),
+                approxfun(Npts, n))
+  nrs_fn = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'NoRes'),
+                approxfun(Npts, n))
+  res_auc = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'Res'),
+                 sum(integrate(res_fn, 0, 1)$value,
+                     integrate(res_fn, 1, max(Npts))$value))
+  nrs_auc = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'NoRes'),
+                 sum(integrate(nrs_fn, 0, 1)$value,
+                     integrate(nrs_fn, 1, max(Npts))$value))
+  d_auc = res_auc - nrs_auc
+  rm_auc_null[i,] = c(d_auc, i)
+  
+}
+
+write.table(rm_auc_null, "../permut_data/mags_remission_auc_null.txt",
+            sep = "\t", row.names = F, quote = F)
---
+++ scripts/permutation_species.R
@@ -0,0 +1,233 @@
+#!/usr/bin/env Rscript
+#Shekarriz Sep11,2023
+
+library(tidyverse)
+library(tidytext)
+
+mapfile <- read.csv("../data/UCFMT1_METAmap.txt", sep = "\t")
+marker_lvl_RA <- read.csv("../data/species_lvl_RA.csv")
+
+
+### donorB species and set engraftment cutoff
+marker_lvl_RA %>%
+  gather(sample, abundance, -Marker) %>%
+  #filtering donor B markers
+  filter(str_detect(sample, "DonorB_D_")) %>%
+  #any marker with abundance > 0
+  #markers that were present in at least one donor B sample
+  filter(!is.na(abundance)) %>%
+  filter(as.numeric(abundance) > 0) %>%
+  mutate(sample= gsub("DonorB_D_", "", sample)) -> donorB_species
+
+mapfile %>% select(Study_ID, Treatment, Timepoint) -> map
+
+##### seting engraftment cut-off
+# based on donor B benchmarking data: 0.005 (as shown above)
+# minimum relative abundance detected maximum (top quartile) of species
+cutoff= 0.005
+######
+
+# working with only donor B species that meeet cutoff
+# > average of minimum detections
+donorB_species %>%
+  filter(as.numeric(abundance) > cutoff) -> donorB_species
+
+
+donorB_species %>%
+  select(Marker) %>% distinct() %>%
+  pull() -> B_species
+
+
+## Functions
+# Make the marker data frame long
+mark_to_long = function(marker_lvl, strains) {
+  # This can be done once. It doesn't need to be repeated each time
+  # convert marker-lvl to long format and make StudyID variable to match mapfile
+  # and select only donor's strains
+  marker_lvl %>%
+    filter(Marker %in% strains) %>%
+    gather(sample, abundance, -Marker) %>%
+    mutate(Study_ID = case_when(
+      str_detect(sample,"PMCL") ~ paste(gsub("_.*", "",sample)),
+      TRUE ~ paste(sample))) -> long_markerlvl
+  return(long_markerlvl)
+}
+
+################################################################################
+# a function to detect common engraftment based on strains (metaphlan markers)
+################################################################################
+get_engraft <- function(long_markerlvl, mapfile, cutoff=cutoff, ...){
+  # complete list of engrafted
+  engraft <- (long_markerlvl 
+              %>% left_join(mapfile) 
+              %>% filter(...)
+              %>% spread(Timepoint, abundance, fill = 0) 
+              %>% filter(WK0 <= 0 & WK6 > cutoff) 
+              %>% select(Marker, Study_ID, WK6) 
+              %>% spread(Study_ID, WK6, fill = 0) 
+              %>% gather(Study_ID, WK6, -Marker)
+              %>% left_join(mapfile))
+  return(engraft)
+}
+
+count_engraft <- function(engraft, group_cols){
+  # Count how many markers engrafted in how many patients
+  g1 = group_cols
+  g2 = c('Marker', group_cols)
+  ct_df = (engraft
+           %>% group_by(.[,g2])
+           %>% summarize(n = sum(WK6 > 0))
+           %>% pivot_wider(names_from = {{ g1 }},
+                           values_from = n)
+           %>% ungroup()
+           %>% mutate(Total = rowSums(across(2:ncol(.)))))
+  # Uncomment the next bit if you want the cumulative sums, but it adds 0.8s
+  # to the run time!
+  # ct_df = (ct_df
+  #          %>% mutate(n = rev(cumsum(rev(n))))) 
+  return(ct_df)
+  
+}
+################################################################################
+
+
+## permutation of treatment FMT vs placebo (n=12)
+# Number of permutations
+num_permutations <- 1000
+it_res = list()
+# Remove unnecessary columns from the mapfile to speed up 
+# the loop (saves 10s per loop)
+map_mod = (mapfile
+           %>% select(Study_ID, Timepoint, Treatment, Fig_lab))
+# Converting the markers to long format only needs to be done once, saves 5s per
+# loop
+markers_long = mark_to_long(marker_lvl_RA, B_species)
+for (i in 1:num_permutations) {
+  # Permute the Treatment column
+  permuted_data <- (map_mod
+                    %>% select(Fig_lab, Treatment)
+                    %>% unique()
+                    %>% mutate(Treatment = sample(Treatment, replace = FALSE))
+                    %>% right_join(select(map_mod, -Treatment))
+                    %>% select(-Fig_lab)) # don't need this anymore
+  it_res[[i]] = list()
+  it_res[[i]][['counts']] = (markers_long
+                             %>% get_engraft(permuted_data, cutoff, 
+                                             Treatment %in% c('FMT', 'Placebo'))
+                             %>% count_engraft(group_cols = 'Treatment')
+                             %>% mutate(Uniqueness = 
+                                          case_when(FMT == 0 & Placebo == 0 ~ 'zero',
+                                                    FMT == 0 ~ 'Placebo',
+                                                    Placebo == 0 ~ 'FMT',
+                                                    TRUE ~ 'Both'),
+                                        it = paste('it', i, sep = '_')))
+  it_res[[i]][['Npts_tx']] = rbind(
+    (it_res[[i]][['counts']]
+     %>% count(FMT)
+     %>% rename(Npts = FMT)
+     %>% mutate(Treatment = 'FMT')),
+    (it_res[[i]][['counts']]
+     %>% count(Placebo)
+     %>% rename(Npts = Placebo)
+     %>% mutate(Treatment = 'Placebo')))
+  it_res[[i]][['Npts_total']] = (it_res[[i]][['counts']]
+                                 %>% count(Total, Uniqueness)
+                                 %>% rename(Npts = Total)
+                                 %>% mutate(Treatment = 'Total'))
+}
+
+### Permuted AUC
+#approxfun creates an approximate function from the data using imputation
+#integrate performs numerical integration to get the area under the curve
+auc_null = matrix(NA, ncol = 2, nrow = num_permutations)
+colnames(auc_null) = c('deltaAUC','iteration')
+
+for (i in 1:num_permutations){
+  fmt_fn = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'FMT'),
+                approxfun(Npts, n))
+  plc_fn = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'Placebo'),
+                approxfun(Npts, n))
+  fmt_auc = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'FMT'),
+                 integrate(fmt_fn, 1, max(Npts))$value)
+  plc_auc = with(filter(it_res[[i]][['Npts_tx']], Treatment == 'Placebo'),
+                 integrate(plc_fn, 1, max(Npts))$value)
+  d_auc = fmt_auc - plc_auc 
+  auc_null[i,] = c(d_auc, i)
+  
+}
+write.table(auc_null, "../permut_data/species_auc_null.txt",
+            sep = "\t", row.names = F, quote = F)
+
+
+## permutations of responders vs non-responders within FMT
+# Number of permutations
+num_permutations <- 1000
+
+fmt_res = list()
+
+# Remove unnecessary columns from the mapfile to speed up the loop (saves 10s per 
+# loop)
+map_mod = (mapfile
+           %>% filter(Treatment == 'FMT')
+           %>% select(Study_ID, Timepoint, Remission, Fig_lab))
+# Loop through permutations
+for (i in 1:num_permutations) {
+  # Permute the Treatment column
+  permuted_data <- (map_mod
+                    %>% select(Fig_lab, Remission)
+                    %>% unique()
+                    %>% mutate(Remission = sample(Remission, replace = FALSE))
+                    %>% right_join(select(map_mod, -Remission))
+                    %>% select(-Fig_lab)) # don't need this anymore
+  fmt_res[[i]] = list()
+  fmt_res[[i]][['counts']] = (markers_long
+                              %>% get_engraft(permuted_data, cutoff, 
+                                              Remission %in% c('Res', 'NoRes'))
+                              %>% count_engraft(group_cols = 'Remission')
+                              %>% mutate(Uniqueness = 
+                                           case_when(Res == 0 & NoRes == 0 ~ 'zero',
+                                                     Res == 0 ~ 'NoRes',
+                                                     NoRes == 0 ~ 'Res',
+                                                     TRUE ~ 'Both'),
+                                         it = paste('it', i, sep = '_')))
+  fmt_res[[i]][['Npts_tx']] = rbind(
+    (fmt_res[[i]][['counts']]
+     %>% count(Res)
+     %>% rename(Npts = Res)
+     %>% mutate(Remission = 'Res')),
+    (fmt_res[[i]][['counts']]
+     %>% count(NoRes)
+     %>% rename(Npts = NoRes)
+     %>% mutate(Remission = 'NoRes')))
+  it_res[[i]][['Npts_total']] = (fmt_res[[i]][['counts']]
+                                 %>% count(Total, Uniqueness)
+                                 %>% rename(Npts = Total)
+                                 %>% mutate(Remission = 'Total'))
+  
+}
+
+### Permuted AUC
+#approxfun creates an approximate function from the data using imputation
+#integrate performs numerical integration to get the area under the curve
+
+rm_auc_null = matrix(NA, ncol = 2, nrow = num_permutations)
+colnames(rm_auc_null) = c('deltaAUC','iteration')
+
+for (i in 1:num_permutations){
+  res_fn = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'Res'),
+                approxfun(Npts, n))
+  nrs_fn = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'NoRes'),
+                approxfun(Npts, n))
+  res_auc = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'Res'),
+                 sum(integrate(res_fn, 0, 1)$value,
+                     integrate(res_fn, 1, max(Npts))$value))
+  nrs_auc = with(filter(fmt_res[[i]][['Npts_tx']], Remission == 'NoRes'),
+                 sum(integrate(nrs_fn, 0, 1)$value,
+                     integrate(nrs_fn, 1, max(Npts))$value))
+  d_auc = res_auc - nrs_auc
+  rm_auc_null[i,] = c(d_auc, i)
+  
+}
+
+write.table(rm_auc_null, "../permut_data/species_remission_auc_null.txt",
+            sep = "\t", row.names = F, quote = F)
